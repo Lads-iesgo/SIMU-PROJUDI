01e6f83c343dc017770cdfa4408149cded584530
halbert nascimento <halbert.fac@gmail.com>
2026-03-18 15:39:00 -0300

    feat: Adicionar redirecionamento para a página de login após logout

--- core/settings.py
+++ core/settings.py
@@ -108,6 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 AUTH_USER_MODEL = "acesso.Usuario" # informa ao Django para usar o modelo de usuário personalizado
+LOGOUT_REDIRECT_URL = 'acesso:login' # redireciona para a página de login após logout
 
 
 # Internationalization
