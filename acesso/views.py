2d5f09a13974a20ec75c11a83c04edb1a13a6e6e
halbert nascimento <halbert.fac@gmail.com>
2026-03-18 15:38:10 -0300

    feat: Atualizar lógica de redirecionamento após login e cadastro de usuário

--- acesso/views.py
+++ acesso/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import login
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
@@ -6,13 +7,31 @@ from django.urls import reverse_lazy
 from .forms import CadastroPublicoForm, LoginForm
 
 
-# class EntrarView(LoginView):
-#     template_name = "acesso/login.html"
-#     authentication_form = LoginForm
 
 
-# class SairView(LogoutView):
-#     next_page = reverse_lazy("acesso:login")
+def login_view(request):
+    form = LoginForm(request, data=request.POST or None)
+    if request.method == "POST" and form.is_valid():
+        login(request, form.get_user()) 
+      
+        # todo: redirecionar para pagina de acordo com perfil
+        if request.user.tipo_perfil_global == "Admin":
+            return redirect("admin:index")
+        
+        if request.user.tipo_perfil_global == "Coordenador":
+            return redirect("acesso:painel_administrativo")
+        
+        if request.user.tipo_perfil_global == "Professor":
+            return redirect("acesso:usuario_lista")
+        
+        if request.user.tipo_perfil_global == "Aluno":
+            return redirect("base:cadastro_processo")
+        
+        return redirect("base:redirecionamento_teste_sucesso")
+    
+    
+    return render(request, "acesso/login.html", {"form": form})
+
 
 
 
@@ -23,7 +42,7 @@ def cadastrar(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect("base:base/home")
+            return redirect("base:redirecionamento_teste_sucesso")
     else:
         form = CadastroPublicoForm()
 
