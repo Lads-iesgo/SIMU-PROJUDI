7f78c5e1b479e892060f40fb398381dc4d31a4ad
halbert nascimento <halbert.fac@gmail.com>
2026-03-18 14:33:32 -0300

    feat: Adicionar rota de login ao arquivo de URLs

--- acesso/urls.py
+++ acesso/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from .views import  cadastrar
+from .views import  cadastrar, login_view
 from .views_admin_usuarios import usuario_atualizar, usuario_lista
 
 app_name = "acesso"
 
 urlpatterns = [
 
+    path("", login_view, name="login"),
     path("cadastro/", cadastrar, name="cadastro"),
 
     # Gestão de usuários
