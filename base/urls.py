f5889165d4035b0cdc851b8afedf783bb0d6842b
halbert-nascimento <halbert.fac@gmail.com>
2026-03-20 17:28:30 -0300

    feat: Atualizar a rota da página inicial para redirecionar para a view de login

--- base/urls.py
+++ base/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
 from .views import home, redirecionamento_teste_sucesso, cadastro_processo
+from acesso.views import login_view
 
 
 app_name = "base"
 
 urlpatterns = [
-    path("home/", home, name="home"),
+    path("home/", login_view, name="home"),
     path("redirecionamento-teste-sucesso/", redirecionamento_teste_sucesso, name="redirecionamento_teste_sucesso"),
     path("cadastro_processo/", cadastro_processo, name="cadastro_processo"),
     
